ecac73f399f47fbab7c5f1f83d904d80f5ce5a8d
Daniel Sansom <daniel.jon.sansom@gmail.com>
2020-04-26 21:04:28 +0200

    Updated system py file.

--- tutorials/file-io/system.py
+++ tutorials/file-io/system.py
@@ -4,6 +4,3 @@ import sys
 defaultEncoding = sys.getdefaultencoding()
 print(defaultEncoding)
 
-text = "this is a line of text\nAnother line of text.\n\nA third line."
-print(text)
-sys.stdout.write(text)
